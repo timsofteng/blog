4bff5becd7dd366344b0e02792c5b8c421fa97f5
beefheart <timofey2009zak@gmail.com>
2019-07-11 23:11:43 +0300

    final version of app

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,18 @@
+import _ from 'lodash';
 import jsonPlacefolder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+        await dispatch(fetchPosts());
+
+        // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+        // userIds.forEach(id => dispatch(fetchUser(id)));
+
+        _.chain(getState().posts)
+            .map('userId')
+            .uniq()
+            .forEach(id => dispatch(fetchUser(id)))
+            .value()
+};
 
 export const fetchPosts = () => async dispatch => {
         const response = await jsonPlacefolder.get('./posts');
@@ -9,6 +23,12 @@ export const fetchPosts = () => async dispatch => {
 
 export const fetchUser = (id) => async dispatch => {
         const response = await jsonPlacefolder.get(`/users/${id}`);
-
         dispatch ({ type: 'FETCH_USER', payload: response.data })
 };
+
+// export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+//
+// const _fetchUser = _.memoize( async (id, dispatch) =>{
+//         const response = await jsonPlacefolder.get(`/users/${id}`);
+//         dispatch ({ type: 'FETCH_USER', payload: response.data })
+// });
